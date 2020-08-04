7e80dc27e57ce193f266d4ef8b68ee91ab235e71
Chris Nelson <mrcc.nelson@googlemail.com>
2020-08-04 19:17:22 +0100

    Initial commit

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 1f;
+    public float gravity = -9.8f;
+    private CharacterController _charController;
+
+    private void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        //get keyboard input * speed
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        // translate to movement
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        // limit diagonal movement to speed of movement
+        movement = Vector3.ClampMagnitude(movement, speed);
+        // apply gravity
+        movement.y = gravity;
+        // framerate independant movement
+        movement *= Time.deltaTime;
+        // transform movement vector from local to gloabl coords
+        movement = transform.TransformDirection(movement);
+        // apply movement via character controller component
+        _charController.Move(movement);
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    private void Start()
+    {
+        // ensure player rotation is only affected by mouse rotation, 
+        // not physics simulation (if applicable)
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            // horizontal rotation
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            // vertical rotation
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            // (vertical rotation goes around x axis)
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float rotationY = transform.localEulerAngles.y;
+            // new vector3 required as transforms are read only
+            // unity converts from euler angles to quaternions
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else
+        {
+            // both 
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            // delta = amount of change
+            // delta holds horizontal rotation and gets added to vertical rotation
+            float rotationY = transform.localEulerAngles.y + delta;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
